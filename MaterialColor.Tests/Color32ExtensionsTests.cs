9d85f6703e49f2bade8ba9a1a73f8e4833c51d40
Szymon Rudziński <swisrud+github@gmail.com>
2017-08-15 21:45:44 +0200

    After Injector references cleanup

--- MaterialColor.Tests/Color32ExtensionsTests.cs
+++ MaterialColor.Tests/Color32ExtensionsTests.cs
@@ -1,10 +1,5 @@
 ﻿using MaterialColor.Extensions;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace MaterialColor.Tests
--- MaterialColor.Tests/JsonTests.cs
+++ MaterialColor.Tests/JsonTests.cs
@@ -1,11 +1,7 @@
 ﻿using Common.Data;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MaterialColor.Tests
 {
