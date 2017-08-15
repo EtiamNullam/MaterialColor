ca9acf035dd62965554e4fa2fcdeba27ce2380fe
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2017-08-15 21:27:51 +0200

    After Common references cleanup

--- Common/Data/Color32Multiplier.cs
+++ Common/Data/Color32Multiplier.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Common.Data
+﻿namespace Common.Data
 {
     public class Color32Multiplier
     {
