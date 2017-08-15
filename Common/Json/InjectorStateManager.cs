cb5de34f9a1bc2da4164c0311e791b251cf3d083
Szymon Rudziński <swisrud+github@gmail.com>
2017-08-15 21:27:51 +0200

    After Common references cleanup

--- Common/Json/InjectorStateManager.cs
+++ Common/Json/InjectorStateManager.cs
@@ -1,11 +1,4 @@
-﻿using Common.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Common.Json
+﻿namespace Common.Json
 {
     public class InjectorStateManager
     {
