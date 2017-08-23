a594deb72291fd62b71a2c2b27ffec391380a652
Szymon Rudziński <swisrud+github@gmail.com>
2017-08-23 18:34:47 +0200

    Extend configurator and refactor injector

--- MaterialColor/Helpers/MaterialHelper.cs
+++ MaterialColor/Helpers/MaterialHelper.cs
@@ -2,6 +2,19 @@
 {
     public static class MaterialHelper
     {
+        public static SimHashes GetMaterialFromCell(int cellIndex)
+        {
+            if (!Grid.IsValidCell(cellIndex))
+            {
+                return SimHashes.Vacuum;
+            }
+
+            var cellElementIndex = Grid.Cell[cellIndex].elementIdx;
+            var element = ElementLoader.elements[cellElementIndex];
+
+            return element.id;
+        }
+
         public static SimHashes ExtractMaterial(KAnimControllerBase kAnimController)
             => kAnimController.GetComponent<PrimaryElement>().ElementID;
     }
