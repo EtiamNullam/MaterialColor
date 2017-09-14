621962c3313540456493420d1820e870494761e1
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2017-09-14 20:10:28 +0200

    Add simple logging for all applications

--- MaterialColor.Common/Paths.cs
+++ MaterialColor.Common/Paths.cs
@@ -4,18 +4,25 @@ namespace MaterialColor.Common
 {
     public static class Paths
     {
-        public static readonly string ElementColorInfosPath = ConfigDirectory + Path.DirectorySeparatorChar + ElementColorInfosFilePath;
-        public static readonly string TypeColorsPath = ConfigDirectory + Path.DirectorySeparatorChar + TypeColorsFilePath;
-        public static readonly string InjectorStatePath = ConfigDirectory + Path.DirectorySeparatorChar + InjectorStateFilePath;
-        public static readonly string MaterialColorStatePath = ConfigDirectory + Path.DirectorySeparatorChar + MaterialColorStateFilePath;
-        public static readonly string OnionStatePath = ConfigDirectory + Path.DirectorySeparatorChar + OnionStateFilePath;
+        public static readonly string ElementColorInfosPath = ConfigDirectory + Path.DirectorySeparatorChar + ElementColorInfosFileName;
+        public static readonly string TypeColorsPath = ConfigDirectory + Path.DirectorySeparatorChar + TypeColorsFileName;
+        public static readonly string InjectorStatePath = ConfigDirectory + Path.DirectorySeparatorChar + InjectorStateFileName;
+        public static readonly string MaterialColorStatePath = ConfigDirectory + Path.DirectorySeparatorChar + MaterialColorStateFileName;
+        public static readonly string OnionStatePath = ConfigDirectory + Path.DirectorySeparatorChar + OnionStateFileName;
 
         public const string ConfigDirectory = "MaterialColorConfig";
 
-        public const string ElementColorInfosFilePath = "ElementColorInfos.json";
-        public const string TypeColorsFilePath = "TypeColorOffsets.json";
-        public const string InjectorStateFilePath = "InjectorState.json";
-        public const string MaterialColorStateFilePath = "MaterialColorState.json";
-        public const string OnionStateFilePath = "OnionState.json";
+        public const string ElementColorInfosFileName = "ElementColorInfos.json";
+        public const string TypeColorsFileName = "TypeColorOffsets.json";
+        public const string InjectorStateFileName = "InjectorState.json";
+        public const string MaterialColorStateFileName = "MaterialColorState.json";
+        public const string OnionStateFileName = "OnionState.json";
+
+        public const string LogsDirectory = "Logs";
+
+        public const string MaterialCoreLogFileName = "MaterialCoreLog.txt";
+        public const string ConfiguratorLogFileName = "ConfiguratorLog.txt";
+        public const string InjectorLogFileName = "InjectorLog.txt";
+        public const string OnionLogFileName = "OnionLog.txt";
     }
 }
