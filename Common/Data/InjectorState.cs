42afca794f24ad2154d2743a90d4be3a77591e02
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2017-09-21 00:10:45 +0200

    Add class for Injector's state

---
+++ Common/Data/InjectorState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Data
+{
+    public class InjectorState
+    {
+        public bool InjectMaterialColor { get; set; } = true;
+        public bool InjectOnion { get; set; } = true;
+        public bool EnableDebugConsole { get; set; } = false;
+    }
+}
