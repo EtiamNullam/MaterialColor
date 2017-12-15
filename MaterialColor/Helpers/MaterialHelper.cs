b99653bd77983f24e8ed0d13db6f65fe7fd025c2
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2017-12-15 10:13:52 +0100

    Cleanup of obsolete methods

--- MaterialColor/Helpers/MaterialHelper.cs
+++ MaterialColor/Helpers/MaterialHelper.cs
@@ -5,53 +5,6 @@ namespace MaterialColor.Helpers
 {
     public static class MaterialHelper
     {
-        // TODO: remove, obsolete
-        public static SimHashes GetMaterialFromCell(int cellIndex)
-        {
-            if (!Grid.IsValidCell(cellIndex))
-            {
-                return SimHashes.Vacuum;
-            }
-
-            return TryCellIndexToSimHash(cellIndex);
-        }
-
-        // TODO: remove, obsolete
-        private static SimHashes TryCellIndexToSimHash(int cellIndex)
-        {
-            try
-            {
-                return CellIndexToSimHash(cellIndex);
-            }
-            catch (Exception e)
-            {
-                State.Logger.Log("Cell or element index from index failed");
-                State.Logger.Log(e);
-            }
-
-            return SimHashes.Vacuum;
-        }
-
-        // TODO: remove, obsolete
-        private static SimHashes CellIndexToSimHash(int cellIndex)
-        {
-            var cell = Grid.Cell[cellIndex];
-
-            var cellElementIndex = cell.elementIdx;
-            var element = ElementLoader.elements[cellElementIndex];
-
-            if (element != null)
-            {
-                return element.id;
-            }
-            else
-            {
-                State.Logger.Log("Element from cell failed.");
-            }
-
-            return SimHashes.Vacuum;
-        }
-
         public static SimHashes ExtractMaterial(Component component)
         {
             var primaryElement = component.GetComponent<PrimaryElement>();
