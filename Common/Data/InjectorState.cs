05c9f9948c82899009da943b92fa774768648e63
Szymon Rudziński <swisrud+github@gmail.com>
2017-12-16 22:52:20 +0100

    Allow for custom max values of gas and liquid sensors too

--- Common/Data/InjectorState.cs
+++ Common/Data/InjectorState.cs
@@ -11,8 +11,12 @@ namespace Common.Data
         public bool InjectMaterialColor { get; set; } = true;
         public bool InjectMaterialColorOverlayButton { get; set; } = true;
         public bool InjectOnion { get; set; } = true;
-        public bool CustomTemperatureSensorRange { get; set; } = true;
+
+        public bool CustomSensorRanges { get; set; } = true;
         public float MaxSensorTemperature { get; set; } = 1273.15f;
+        public float MaxGasSensorPressure { get; set; } = 25;
+        public float MaxLiquidSensorPressure { get; set; } = 10000;
+
         public bool InjectRemoteDoors { get; set; } = false;
         public bool EnableDebugConsole { get; set; } = false;
     }
